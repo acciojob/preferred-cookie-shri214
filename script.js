eac949360fc93fdaeb2fc7762c4d5b1dde2facd9
Shriram kushwaha <117708615+shri214@users.noreply.github.com>
2023-04-24 13:32:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function savePreferences() {
+  const fontSize = document.getElementById('fontsize').value;
+  const color = document.getElementById('fontcolor').value;
+  
+  document.body.style.fontSize = fontSize + 'px';
+  document.body.style.color = color;
+  
+  // Store preferences as cookies
+  document.cookie = 'font-size=' + fontSize ;
+  document.cookie = 'color=' + color;
+}
